67f1dbbfb16ff45154983f364d1a379f83cbda91
bwissal13 <baazizwissal13@gmail.com>
2024-11-19 22:04:05 +0100

    add project structure

---
+++ src/test/java/org/example/citronix/CitronixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.citronix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitronixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
