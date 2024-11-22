5400e5327cc7b4acefe7b40fc5d9219abd0b0b54
bwissal13 <baazizwissal13@gmail.com>
2024-11-22 10:27:32 +0100

    Update Arbre Mapper

--- src/main/java/org/example/citronix/mapper/ArbreMapper.java
+++ src/main/java/org/example/citronix/mapper/ArbreMapper.java
@@ -3,13 +3,15 @@ package org.example.citronix.mapper;
 import org.example.citronix.dto.ArbreDTO;
 import org.example.citronix.entity.Arbre;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.example.citronix.utility.AgeCalculator;
+import org.example.citronix.utility.ProductivityCalculator;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface ArbreMapper {
     ArbreMapper INSTANCE = Mappers.getMapper(ArbreMapper.class);
-
-    Arbre toEntity(ArbreDTO arbreDTO);
-
+    @Mapping(source = "champ.id", target = "champId")
     ArbreDTO toDTO(Arbre arbre);
+    Arbre toEntity(ArbreDTO arbreDTO);
 }
