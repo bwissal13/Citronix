e1bdfe95810508c5b276757af756fc209d44cd4d
bwissal13 <baazizwissal13@gmail.com>
2024-11-24 15:28:42 +0100

    Update Recolte Mapper

--- src/main/java/org/example/citronix/mapper/RecolteMapper.java
+++ src/main/java/org/example/citronix/mapper/RecolteMapper.java
@@ -3,13 +3,12 @@ package org.example.citronix.mapper;
 import org.example.citronix.dto.RecolteDTO;
 import org.example.citronix.entity.Recolte;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-
-@Mapper
+@Mapper(componentModel = "spring")
 public interface RecolteMapper {
     RecolteMapper INSTANCE = Mappers.getMapper(RecolteMapper.class);
-
+    @Mapping(source = "champID", target = "champ.id")
     Recolte toEntity(RecolteDTO recolteDTO);
-
     RecolteDTO toDTO(Recolte recolte);
 }
