62eb9082171472bf306fc12a44b868892f8bb5e5
bwissal13 <baazizwissal13@gmail.com>
2024-11-24 15:30:10 +0100

    Update Recolte Controller

--- src/main/java/org/example/citronix/controller/RecolteController.java
+++ src/main/java/org/example/citronix/controller/RecolteController.java
@@ -1,8 +1,11 @@
 package org.example.citronix.controller;
 
+import lombok.AllArgsConstructor;
 import org.example.citronix.dto.RecolteDTO;
+import org.example.citronix.dto.DetailRecolteDTO;
+import org.example.citronix.service.detailRecolte.DetailRecolteService;
+import org.example.citronix.service.detailRecolte.DetailRecolteServiceImpl;
 import org.example.citronix.service.recolte.RecolteService;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,30 +13,47 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/recoltes")
+@RequestMapping("api/recoltes")
+@AllArgsConstructor
 public class RecolteController {
 
     private final RecolteService recolteService;
-
-    public RecolteController(RecolteService recolteService) {
-        this.recolteService = recolteService;
-    }
+    private final DetailRecolteService detailRecolteService;
 
     @PostMapping
     public ResponseEntity<RecolteDTO> ajouterRecolte(@RequestBody RecolteDTO recolteDTO) {
-        RecolteDTO createdRecolte = recolteService.ajouterRecolte(recolteDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdRecolte);
+        RecolteDTO savedRecolte = recolteService.ajouterRecolte(recolteDTO);
+        return new ResponseEntity<>(savedRecolte, HttpStatus.CREATED);
     }
 
-
-    @GetMapping("/champ/{champId}")
-    public ResponseEntity<List<RecolteDTO>> getRecoltesByChamp(@PathVariable Long champId) {
-        return ResponseEntity.ok(recolteService.getRecoltesByChamp(champId));
+    @GetMapping
+    public ResponseEntity<List<RecolteDTO>> getAllRecoltes() {
+        List<RecolteDTO> recoltes = recolteService.getAllRecoltes();
+        return new ResponseEntity<>(recoltes, HttpStatus.OK);
     }
-
 
     @GetMapping("/{id}")
     public ResponseEntity<RecolteDTO> getRecolteById(@PathVariable Long id) {
-        return ResponseEntity.ok(recolteService.getRecolteById(id));
+        RecolteDTO recolteDTO = recolteService.getRecolteById(id);
+        return new ResponseEntity<>(recolteDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RecolteDTO> updateRecolte(@PathVariable Long id, @RequestBody RecolteDTO recolteDTO) {
+        recolteDTO.setId(id);
+        RecolteDTO updatedRecolte = recolteService.updateRecolte(id,recolteDTO);
+        return new ResponseEntity<>(updatedRecolte, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRecolte(@PathVariable Long id) {
+        recolteService.supprimerRecolte(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/{id}/details")
+    public ResponseEntity<List<DetailRecolteDTO>> ajouterDetailsRecolte(@PathVariable Long id, @RequestBody DetailRecolteDTO recolteDTO) {
+       List<DetailRecolteDTO> details = detailRecolteService.ajouterDetailsRecolte(recolteDTO);
+        return new ResponseEntity<>(details, HttpStatus.CREATED);
     }
 }
