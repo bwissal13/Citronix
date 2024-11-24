7a78f205080e7884bd4f836c3bf7d289d9b49190
bwissal13 <baazizwissal13@gmail.com>
2024-11-24 15:29:03 +0100

    Update Recolte service

--- src/main/java/org/example/citronix/service/recolte/RecolteService.java
+++ src/main/java/org/example/citronix/service/recolte/RecolteService.java
@@ -1,13 +1,16 @@
 package org.example.citronix.service.recolte;
 
 import org.example.citronix.dto.RecolteDTO;
+import org.example.citronix.dto.DetailRecolteDTO;
 
 import java.util.List;
 
 public interface RecolteService {
+
     RecolteDTO ajouterRecolte(RecolteDTO recolteDTO);
-    List<RecolteDTO> getRecoltesByChamp(Long champId);
+    List<RecolteDTO> getAllRecoltes();
     RecolteDTO getRecolteById(Long id);
-//    RecolteDTO modifierRecolte(Long id, RecolteDTO recolteDTO);
-//    void supprimerRecolte(Long id);
+    RecolteDTO updateRecolte(Long id, RecolteDTO recolteDTO);
+    void supprimerRecolte(Long id);
+
 }
